c833174f2d064419c31a06f27c751695c50323b5
Franklin <franklin.alves.oliveira@gmail.com>
2019-01-18 20:52:11 -0200

    Changes in notebook: Included/changed some comments (in the whole notebook). Cleaned up the code a little bit (removed some duplicated code). Also, for the portfolio analysis section, I changed the the code for the Portfolio Weights chart (aesthetics). Changes in Poloniex_franklin.py: added minor comments.

--- Poloniex_franklin.py
+++ Poloniex_franklin.py
@@ -11,6 +11,8 @@ def get_data(list_of_currencies, start, end, freq):
     - end (string): final date in the format YYYY-mm-dd
     - freq (integer): 300 - 5min; 900 - 15min; 1800 - 30min; 7200 - 2h; 14400 - 4h; 86400 - daily.
     '''
+    
+    # necessary imports (it's replicated from the notebook here only for precaution)
     import datetime
     import time
     import pandas as pd
@@ -20,6 +22,7 @@ def get_data(list_of_currencies, start, end, freq):
     from IPython.display import clear_output
     
     
+    # treating inputs
     freq = str(freq)
     since = int(time.mktime(datetime.datetime.strptime(start, "%Y-%m-%d").timetuple()))
     until = int(time.mktime(datetime.datetime.strptime(end, "%Y-%m-%d").timetuple()))
@@ -27,20 +30,34 @@ def get_data(list_of_currencies, start, end, freq):
   
     
     for name in list_of_currencies:
+        
+        # Treating string to be inserted into the url
         name1=name.split('/')
         name1.reverse()
         name=str(name1[0]+'_'+name1[1])
+        
+        # displays which currency data is being downloaded
         print('\033[0m Downloading \033[1m {} \033[0m from {}, since \033[1m {}, \033[0m until \033[1m {}.\033[0m \n'.format(name, 'Poloniex', start, end))
         
+        # fetching data with some error treatment
         try: 
+            
+            # creating the url
             url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'.format(name, since, until, freq)
+            
+            # reading data from Poloniex's (available in .json)
             data = pd.read_json(url)
             data.set_index('date', inplace=True)
             data['currency']='{}'.format(name)
+            
+            # stores data downloaded into a .csv file (with currency name)
             data.to_csv(name+'.csv', header=True, mode='a+')
+            
+            # waiting before next iteration... (Poloniex has an upper limit of data that can be downloaded by the same IP address per minute)
             time.sleep(1.5)
             clear_output(True)
         
+        # passing some exceptions
         except Exception as e:
             print('\033[91m',e,'\033[0m \n')
             time.sleep(1.5)
@@ -52,7 +69,8 @@ def get_data(list_of_currencies, start, end, freq):
             time.sleep(1.5)
             #clear_output(True)
             pass
-        
+    
+    # displays that the download is complete and the process is finished.
     return print('\033[1m Download complete. Check your work directory. \033[0m')
 
 
@@ -61,14 +79,17 @@ def get_data(list_of_currencies, start, end, freq):
 def call_data(list_of_curr_downloaded):
     
     '''
-    Read CSV files of the currencies downloaded and return one data frame
+    Read CSV files of the currencies downloaded and returns one concatenated data frame
     
-    p.s.: Do not run this function to read a large number of CSV files.
+    p.s.: Do not run this function to read a large number of CSV files. 
+        (memmory issues. I didn't solve this because, as we're analyzing only 3 currencies, it's not reeeeeeally necessary. 
+        Also, because I didn't have much time before the hand-in date was due. XD)
     
     '''
     
     import pandas as pd
     
+    # variable to store the "large" data frame
     df = pd.DataFrame()
 
     for name in list_of_curr_downloaded:
@@ -76,14 +97,20 @@ def call_data(list_of_curr_downloaded):
         name1=name.split('/')
         name1.reverse()
         name=str(name1[0]+'_'+name1[1])
-
+        
+        # reads the .csv file
         df1 = pd.read_csv('{}.csv'.format(name))
+        
+        # concatenates each dataframe into a large one
         dataf = pd.concat([df,df1])
         df=dataf
         
+    # sets the index to be the date     
     df.set_index('date', inplace=True)
     
+    # returns the data frame that contains all the data from the .csv files
     return(df)
+
 
 
 
@@ -96,15 +123,18 @@ def data_to_sql(data_frame, name_of_db):
     import pandas.io.sql as psql
     from sqlite3 import Error
    
-    
+    # creates a connection with the database
     connection = sqlite3.connect('{}.db'.format(name_of_db))
     
+    # stores a dataframe into the .db file 
     try:
         pd.DataFrame.to_sql(self = data_frame, name = name_of_db, con = connection, index = True, if_exists = 'replace')
-        
+    
+    # if there's an error, it'll be displayed on the screen
     except Exception as e:
         print('\033[91m{}\033[0m \n'.format(e))
-        
+    
+    # closes the connection with the database 
     connection.close()
     
     
---
+++ README.md
@@ -0,0 +1,13 @@
+# Final work presented in EMAp-FGV summer course - 2018
+
+The main folder (Verao_2018) contains:
+
+- Poloniex_franklin.py: a .py file which contains some code I wrote to: collect data information on a cryptocurrency from POLONIEX, store this data into .csv files and store pandas data frames into a SQL .db file. 
+
+- POLONIEX - 3 currencies - Franklin.ipynb: a notebook which explains, step by step, how each requirement, from the list below, was addressed:
+
+    1. Data on 3 cryptocurrencies should be collected (p.s.: I've chosen to use Poloniex's API);
+    2. A SQL database should be used to store the data. 
+    3. Read information from the SQL database and present it in 5 different graphs. 
+
+P.S.: this work was assigned as a requirement for approval in the summer course in Introduction to Programming with the Python Language -  [EMAp, FGV](https://emap.fgv.br/) (2018).
